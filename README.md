6e0d3fbc2c7378ac2b834508dbb1e53d4610652b
rahnakesume <rahnakesume@gmail.com>
2025-07-02 09:38:54 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bokepdikolam
-bokepdikolam
+# [bokepdikolam](https://bokepdikolam.pages.dev)
---
+++ src/utils/site.ts
@@ -0,0 +1,4 @@
+export const nama = 'Bokep Dikolam';
+export const desk = 'Video Bokep Indo Jepang Abg Montok Lokal Tante Viral Terbaru Bocil Ngentot Jilbab Smp Colmek';
+export const url = 'https://bokepdikolam.pages.dev';
+export const terbit = '2025-05-16T00:01:03Z';
